e456d7266877e97f1ef64a5ca5a69242fa3d0cce
Ivan Podoprigorov <ivan.podoprigorov@mail.ru>
2025-03-27 18:02:54 +0500

    Создание страниц Настроек и Профиля пользователя

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 import { useEffect } from "react";
 
 import { Loader } from "lucide-react";
@@ -17,7 +18,7 @@ import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
-
+  const {theme}=useThemeStore()
   useEffect(() => {
     checkAuth()
   }, [checkAuth]);
@@ -29,11 +30,10 @@ const App = () => {
     <div className="flex items-center justify-center h-screen">
       <Loader className="size-10 animate-spin" />
     </div>
-  )
+  );
 
   return (
-  <div>
-
+  <div data-theme={theme}>
     <Navbar />
 
     <Routes>
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,4 +1,105 @@
+import { Camera, Mail, User } from "lucide-react";
+import { useAuthStore } from "../store/useAuthStore";
+import { useState } from "react";
+
 const ProfilePage = () => {
-    return <div>ProfilePage</div>;
+    const { authUser, isUpdatingProfile, updateProfile }=useAuthStore()
+    const [selectedImg, setSelectedImg] = useState(null);
+    
+    const handleImageUpload = async(e) => {
+        const file = e.target.files[0];
+        if (!file) return;
+    
+        const reader = new FileReader();
+    
+        reader.readAsDataURL(file);
+    
+        reader.onload = async () => {
+          const base64Image = reader.result;
+          setSelectedImg(base64Image);
+          await updateProfile({ profilePic: base64Image });
+        };
+    };
+
+
+    return (
+        <div className="h-screen pt-20">
+            <div className="max-w-2xl p-4 py-8 mx-auto">
+                <div className="p-6 space-y-8 bg-base-300 rounded-xl">
+                    <div className="text-center">
+                        <h1 className="text-2xl font-semibold">Профиль</h1>
+                        <p className="mt-2">Информация о вас</p>
+                    </div>
+                    
+                    {/*секция обновления аватарки*/}
+                    <div className="flex flex-col items-center gap-4">
+            <div className="relative">
+              <img
+                src={selectedImg || authUser.profilePic || "/avatar.png"}
+                alt="Profile"
+                className="object-cover border-4 rounded-full size-32 "
+              />
+              <label
+                htmlFor="avatar-upload"
+                className={`
+                  absolute bottom-0 right-0 
+                  bg-base-content hover:scale-105
+                  p-2 rounded-full cursor-pointer 
+                  transition-all duration-200
+                  ${isUpdatingProfile ? "animate-pulse pointer-events-none" : ""}
+                `}
+              >
+                <Camera className="w-5 h-5 text-base-200" />
+                <input
+                  type="file"
+                  id="avatar-upload"
+                  className="hidden"
+                  accept="image/*"
+                  onChange={handleImageUpload}
+                  disabled={isUpdatingProfile}
+                />
+              </label>
+            </div>
+            <p className="text-sm text-zinc-400">
+              {isUpdatingProfile ? "Загрузка..." : "Нажмите на иконку камеры что бы обновить ваше фото"}
+            </p>
+          </div>
+          
+          <div className="space-y-6">
+            <div className="space-y-1.5">
+              <div className="flex items-center gap-2 text-sm text-zinc-400">
+                <User className="w-4 h-4" />
+                    Имя
+              </div>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.fullName}</p>
+            </div>
+
+            <div className="space-y-1.5">
+              <div className="flex items-center gap-2 text-sm text-zinc-400">
+                <Mail className="w-4 h-4" />
+                Почта
+              </div>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.email}</p>
+            </div>
+          </div>
+          <div className="p-6 mt-6 bg-base-300 rounded-xl">
+            <h2 className="mb-4 text-lg font-medium">Информация об аккаунте</h2>
+            <div className="space-y-3 text-sm">
+              <div className="flex items-center justify-between py-2 border-b border-zinc-700">
+                <span>Участник с</span>
+                <span>{authUser.createdAt?.split("T")[0]}</span>
+              </div>
+              <div className="flex items-center justify-between py-2">
+                <span>Статус </span>
+                <span className="text-green-500">Онлайн</span>
+              </div>
+            </div>
+          </div>
+                </div>
+                
+            </div>
+
+        </div>
+    )
 };
 export default ProfilePage;
